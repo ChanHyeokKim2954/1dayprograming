27601f6d1a6ee21804be11276d776863c9067852
ChanHyeokKim2954 <101760476+ChanHyeokKim2954@users.noreply.github.com>
2022-09-14 00:26:58 +0900

    Add files via upload

---
+++ 20220913/MovingAverageFilter/202011138_Moving_Average_Filter.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#define DATA_SIZE 5
+
+double array[DATA_SIZE] = {0.0,};
+
+int Input_Data(void)
+{
+	double data = 0;
+
+	printf("INPUT DATA : ");
+	scanf_s("%lf", &data);
+
+	return data;
+}
+
+double MovingAverageFilter(double average_old, double data)
+{
+	double average = 0.0;
+
+	average = average_old + (data - array[0]) / DATA_SIZE;
+
+	return average;
+}
+
+void Show_Data(double calculation)
+{
+	printf("DATA_Array = ");
+	for (int i = 0; i < DATA_SIZE; i++)	printf("%.2lf ", array[i]);
+	printf("\n");
+	printf("Average : %.2lf \n\n", calculation);
+}
+
+int main(void)
+{
+	int count = 1;
+	double average_old = 0.0;
+	double calculation = 0.0;
+	while(1)
+	{
+		double data = Input_Data();
+		array[count] = data;
+		average_old = calculation;
+		//printf("Average_old : %.2lf \n\n", average_old);
+		calculation = MovingAverageFilter(average_old,data);
+		Show_Data(calculation);
+		count++;
+
+		if (count >= DATA_SIZE)
+		{
+			count = 4;
+			for (int i = 0; i < DATA_SIZE - 1; i++)	array[i] = array[i + 1];
+		}
+	}
+
+	return 0;
+}
