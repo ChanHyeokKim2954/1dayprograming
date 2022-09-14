ff2ca66734faaa62c7995ac62ddec953908d87c0
ChanHyeokKim2954 <101760476+ChanHyeokKim2954@users.noreply.github.com>
2022-09-14 16:49:40 +0900

    Update and rename 202011138_Moving_Average_Filter.cpp to 202011138_Average_Filter.cpp

---
+++ 20220913/MovingAverageFilter/202011138_Average_Filter.cpp
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+double array[] = {0.0, };
+
+int Input_Size(void)
+{
+	int size = 0;
+	printf("INPUT DATA SIZE : ");
+	scanf_s("%d", &size);
+	printf("=================================\n");
+	return size;
+}
+double Input_Data(void)
+{
+	double data = 0.0;
+	printf("INPUT DATA : ");
+	scanf_s("%lf", &data);
+
+	return data;
+}
+double PreAverage(int size)
+{
+	double sum = 0.0;
+	for (int i = 0; i < size - 1; i++)	sum += array[i];
+	return sum / ((double)size - 1);
+}
+
+double AverageFilter(double average_old, double data, int size)
+{
+	double average = 0.0;
+	double alpha = 0.0;
+	
+	alpha = ((double)size - 1) / (double)size;
+	//printf("alpha : %.2lf \n\n", alpha);
+
+	average = (alpha * average_old) + ((1 - alpha) * data);
+
+	return average;
+}
+
+void Show_Data(double calculation, int size)
+{
+	printf("DATA_Array = ");
+	for (int i = 0; i < size; i++)	printf("%.2lf ", array[i]);
+	printf("\n");
+	printf("Average : %.2lf \n\n", calculation);
+}
+
+int main(void)
+{
+	int count = 0;
+	double average_old = 0.0;
+	double calculation = 0.0;
+
+	int size = Input_Size();
+	while (1)
+	{
+		double data = Input_Data();
+		array[count] = data;
+		calculation = AverageFilter(average_old, data, size);
+		average_old = PreAverage(size);
+		Show_Data(calculation, size);
+		//printf("Average_old : %.2lf \n\n", average_old);
+		count++;
+
+		if (count >= size) break;
+	}
+
+	return 0;
+}
